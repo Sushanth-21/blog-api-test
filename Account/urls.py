0ed16730249af3cd84314dd0a2a0de0f7bce6290
SushanthEnukonda <enukondasushanthreddy21@gmail.com>
2022-03-05 18:29:57 +0530

    final

--- Account/urls.py
+++ Account/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import EmailTokenObtainPairView
 
 urlpatterns = [
-    path('authenticate/', EmailTokenObtainPairView.as_view(),
+    path('authenticate', EmailTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
